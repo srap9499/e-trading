5b9dcc20f140e796da0c7970cedf99a4b313e931
Siddhesh Panchal <59734506+srap9499@users.noreply.github.com>
2021-05-24 11:16:15 +0530

    Added basic products display on SignIn

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,11 +1,16 @@
 'use strict';
 
+const { Product } = require('../models/product.model');
 
 exports.getHome = async (req, res, next) => {
     const user = req.userData;
     const title = "E-Trading";
+    const products = await Product.findAll({
+        attributes: ['id', 'name', 'quantity', 'price']
+    });
     res.render('home', {
         title,
-        user
+        user,
+        products: products
     });
 };
