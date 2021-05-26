b2bda39977b313922cd7eddd1e57ab5bddb3eb5b
Siddhesh Panchal <59734506+srap9499@users.noreply.github.com>
2021-05-26 16:02:26 +0530

    Minor fixes

--- middlewares/validate.middleware.js
+++ middlewares/validate.middleware.js
@@ -3,7 +3,7 @@
 const { joiErrorFormatter } = require("../helpers/error-formatter.helper");
 
 exports.validate = (schema , { target="body", view, title="E-Trading"}) => {
-    target = target.toLocaleLowerCase();
+    target = target.toLowerCase();
     return async (req, res, next) => {
         const validateResult = schema.validate(req[target], {
             abortEarly: false
