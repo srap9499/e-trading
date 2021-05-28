0cd4591224ff6dcb3451025a4ad84d435ff564aa
Siddhesh Panchal <59734506+srap9499@users.noreply.github.com>
2021-05-28 10:27:24 +0530

    Modified products display on home page

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -4,17 +4,42 @@ const {
     getProductPagination,
     getProductPaginationData
 } = require('../helpers/pagination.helper');
+const { User } = require('../models/user.model');
 const { Product } = require('../models/product.model');
+const { Brand } = require('../models/brand.model');
+const { Category, Subcategory } = require('../models/categories.model');
 
 exports.getHome = async (req, res, next) => {
-    const user = req.userData;
+    const { userData } = req;
     const title = "E-Trading";
     const { page, size } = req.query;
     const { limit, offset } = getProductPagination({ page, size });
+    const user = await User.findOne({
+        logging: false,
+        attributes: ["id", "userName", "email"],
+        where: {
+            email: userData.email
+        },
+    });
     const data = await Product.findAndCountAll({
+        logging: false,
         attributes: ['id', 'name', 'quantity', 'price'],
         limit,
-        offset
+        offset,
+        include: [
+            {
+                model: Brand,
+                attributes: ["name"]
+            },
+            {
+                model: Category,
+                attributes: ["category"]
+            },
+            {
+                model: Subcategory,
+                attributes: ["subcategory"]
+            }
+        ]
     });
     let renderData = getProductPaginationData({ data, page, limit });
     renderData.title = title;
