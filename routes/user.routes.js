0fb82c9ad5e160da0ec55457ad00d7138645080c
Siddhesh Panchal <59734506+srap9499@users.noreply.github.com>
2021-05-28 09:17:58 +0530

    Minor fixes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const { Router } = require("express");
-const { getCart, getMyCart } = require("../controllers/user.controller");
+const User = require("../controllers/user.controller");
 const { authenticate } = require('../middlewares/auth.middleware');
 
 const router = Router();
 
-router.get('/mycart', authenticate, getMyCart);
+router.get('/mycart', authenticate, User.getMyCart);
 
 module.exports = router;
