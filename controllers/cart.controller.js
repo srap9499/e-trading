ac23c986bcd03861cc08e7cb14c1c7cf2a5fac61
Siddhesh Panchal <59734506+srap9499@users.noreply.github.com>
2021-05-29 18:47:13 +0530

    Controller function for add to cart

---
+++ controllers/cart.controller.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const { sequelize } = require("../config/db-connection.config");
+
+exports.addToCart = async (req, res, next) => {
+    const { userData } = req;
+    const userId = userData.id;
+    const { productId, quantity } = req.body;
+    try {
+        await sequelize.query('CALL add_to_cart( :userId, :productId, :quantity)', {
+            replacements: { userId, productId, quantity },
+            logging: false
+        });
+        return res.status(200).send("SuccessFull!");
+    } catch (e) {
+        console.log(e);
+        return res.status(500).send("Something went wrong!");
+    }
+};
