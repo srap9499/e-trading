475f823df72a4f96e40dbb93b780b0c34b8d7a11
Siddhesh Panchal <59734506+srap9499@users.noreply.github.com>
2021-06-22 16:12:46 +0530

    Validation schema for Update user profile (Change Password & Update User Details)

--- helpers/validate.helper.js
+++ helpers/validate.helper.js
@@ -42,13 +42,61 @@ const verifySchema = Joi.object({
 
 const addAmountSchema = Joi.object({
     amount: Joi.number()
-    .min(0)
-    .required()
+        .min(0)
+        .required()
 });
+
+const changePasswordSchema = Joi.object({
+    password: Joi.string()
+        .min(5)
+        .max(15)
+        .required(),
+    new_password: Joi.string()
+        .min(5)
+        .max(15)
+        .required(),
+    confirm_password: Joi.ref('new_password')
+})
+    .with('new_password', 'confirm_password');
+
+const changeUserDetailSchema = Joi.object({
+    name: Joi.string()
+        .trim()
+        .min(2)
+        .max(64)
+        .required(),
+
+    email: Joi.string()
+        .trim()
+        .lowercase()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+})
+    .or('name', 'email');
+
+const verifyChangeUserDetailSchema = Joi.object({
+    name: Joi.string()
+        .trim()
+        .min(2)
+        .max(64)
+        .required(),
+
+    email: Joi.string()
+        .trim()
+        .lowercase()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+    otp: Joi.string()
+        .alphanum()
+        .length(6)
+        .required()
+})
+    .or('name', 'email');
 
 module.exports = {
     signUpSchema,
     verifySchema,
     signInSchema,
-    addAmountSchema
+    addAmountSchema,
+    changePasswordSchema,
+    changeUserDetailSchema,
+    verifyChangeUserDetailSchema
 };
