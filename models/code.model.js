7ca50f52b3e5038755c5f511cc952f79b7492296
Siddhesh Panchal <59734506+srap9499@users.noreply.github.com>
2021-06-22 16:09:07 +0530

    Created VerifyCode model

--- models/code.model.js
+++ models/code.model.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { NOW } = require('sequelize');
 const Sequelize = require('sequelize');
 
 const { sequelize } = require('../config/db-connection.config');
@@ -21,4 +22,35 @@ const Code = sequelize.define('code', {
     timestamps: false
 });
 
-module.exports= { Code };
+const VerifyCode = sequelize.define('verifycode', {
+    email: {
+        type: Sequelize.STRING(50),
+        allowNull: false,
+        validate: {
+            isEmail: true,
+        }
+    },
+    otp: {
+        type: Sequelize.STRING(6),
+        allowNull: false,
+    },
+    for: {
+        type: Sequelize.STRING(32),
+        allowNull: false,
+        defaultValue: 'account_activation'
+    },
+    status: {
+        type: Sequelize.ENUM('used', 'unused'),
+        allowNull: false,
+        defaultValue: 'unused',
+    },
+    notAfter: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: NOW()
+    }
+}, {
+    timestamps: false
+});
+
+module.exports= { Code, VerifyCode };
