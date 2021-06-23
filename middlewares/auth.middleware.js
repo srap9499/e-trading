f61d657afbbb28bcf669784a7c91ce2ea620c91a
Siddhesh Panchal <59734506+srap9499@users.noreply.github.com>
2021-06-23 20:05:39 +0530

    Created middleware functions to check user roles

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,17 +1,22 @@
 'use strict';
 
+const { Unauthorized } = require('http-errors');
 // import jwt
 const { verify } = require('jsonwebtoken');
 
-// import environment variables
-require('dotenv').config();
-const JWT_SECRET = process.env.JWT_SECRET;
-const { User } = require('../models/user.model');
+// import config
+const {
+    development: {
+        jwt_secret,
+        roles,
+        errMsgs
+    }
+} = require('../config/development.config');
 
 
 const auth = token => {
     return new Promise((resolve, reject) => {
-        verify(token, JWT_SECRET, (err, data) => {
+        verify(token, jwt_secret, (err, data) => {
             if (err) {
                 reject(err);
             } else {
@@ -36,3 +41,42 @@ exports.authenticate = async (req, res, next) => {
             return res.status(401).redirect('/auth/signIn');
         })
 };
+
+exports.isAdmin = async (req, res, next) => {
+    const { roleId } = req.userData;
+    try {
+        const isAdmin = parseInt(roleId) == parseInt(roles.SubAdmin) || parseInt(roleId) == parseInt(roles.SuperAdmin);
+        if (!isAdmin) {
+            throw new Unauthorized(errMsgs.auth401);
+        }
+        next();
+    } catch (error) {
+        next(error);
+    }
+};
+
+exports.isSuperAdmin = async (req, res, next) => {
+    const {roleId} = req.userData;
+    try {
+        const isSuperAdmin = parseInt(roleId) == parseInt(roles.SuperAdmin);
+        if (!isSuperAdmin) {
+            throw new Unauthorized(errMsgs.auth401);
+        }
+        next();
+    } catch (error) {
+        next(error);
+    }
+};
+
+exports.isUser = async (req, res, next) => {
+    const {roleId} = req.userData;
+    try {
+        const isUser = parseInt(roleId) == parseInt(roles.User);
+        if (!isUser) {
+            throw new Unauthorized(errMsgs.auth401);
+        }
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
