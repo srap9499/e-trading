a1fac7694af1e73df3bcc8880106637031e1940d
Siddhesh Panchal <59734506+srap9499@users.noreply.github.com>
2021-06-23 20:01:56 +0530

    Created validation schema for Add Sub Admin

--- helpers/validate.helper.js
+++ helpers/validate.helper.js
@@ -2,6 +2,26 @@
 
 const Joi = require('joi');
 
+const addSubAdminSchema = Joi.object({
+    userName: Joi.string()
+        .trim()
+        .min(2)
+        .max(64)
+        .required(),
+
+    email: Joi.string()
+        .trim()
+        .lowercase()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+
+    password: Joi.string()
+        .min(5)
+        .max(15)
+        .required(),
+    confirm_password: Joi.ref("password")
+})
+    .with("password", "confirm_password");
+
 const signUpSchema = Joi.object({
     userName: Joi.string()
         .trim()
@@ -92,6 +112,7 @@ const verifyChangeUserDetailSchema = Joi.object({
     .or('name', 'email');
 
 module.exports = {
+    addSubAdminSchema,
     signUpSchema,
     verifySchema,
     signInSchema,
