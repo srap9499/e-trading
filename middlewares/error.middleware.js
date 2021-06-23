df0aff074c43af4d2e23823efe1b01f559412d0b
Siddhesh Panchal <59734506+srap9499@users.noreply.github.com>
2021-06-23 20:25:23 +0530

    Added new error handler middleware

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -1,7 +1,13 @@
 'use strict';
 
-const e = require("express");
+const {
+    development: {
+        errMsgs
+    }
+} = require('../config/development.config');
+const createHttpError = require("http-errors");
 const { sequelizeErrorFormatter } = require("../helpers/error-formatter.helper");
+const { responseObj } = require("../helpers/response.helper");
 
 exports.error = ({ error, view, title="E-Trading"}, req, res, next) => {
     if (error.name && error.name === 'SequelizeValidationError') {
@@ -25,4 +31,27 @@ exports.error = ({ error, view, title="E-Trading"}, req, res, next) => {
         formData: req.body,
         title
     });
+};
+
+exports.errorHandler = async (error, req, res, next) => {
+    // res.status(error.status).send(error);
+    const isHttpError = createHttpError.isHttpError(error);
+    if (isHttpError) {
+        return res.status(error.status).send(responseObj(false, error.message));
+    }
+    switch (error.name) {
+        case "SequelizeValidationError":
+            res.status(400).send(
+                responseObj(false, errMsgs.invalid400, sequelizeErrorFormatter(error))
+            );
+            break;
+    
+        default:
+            const code = error.status??500;
+            console.log(error.name, error.message);
+            res.status(code).send(
+                responseObj(false, code==500 ? errMsgs.err500 : error.message, error)
+            );
+            break;
+    }
 };
