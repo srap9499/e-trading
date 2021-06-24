b38bfd26e07f0da054f85fb786ee82b210c4b3a9
Siddhesh Panchal <59734506+srap9499@users.noreply.github.com>
2021-06-24 20:03:51 +0530

    Changes in Cancel order controller

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -445,32 +445,8 @@ exports.cancelOrder = async (req, res, next) => {
     const userId = userData.id;
     const { orderId } = req.params;
     try {
-        const result = await sequelize.transaction(async cancelTransaction => {
-            const user = await User.findOne({
-                logging: false,
-                attributes: [ 'id', 'userName', 'email' ],
-                where: {
-                    id: userId
-                },
-                transaction: cancelTransaction
-            });
-            const order = await Order.findOne({
-                logging: false,
-                where: {
-                    userId,
-                    id: orderId
-                },
-                include: {
-                    model: OrderDetail,
-                    attributes: [ 'quantity' ],
-                    include: {
-                        model: Product,
-                        attributes: [ 'id', 'quantity' ],
-                    }
-                },
-                transaction: cancelTransaction
-            });
-            const cancelOrderResult = await Order.update({
+        await sequelize.transaction(async cancelTransaction => {
+            await Order.update({
                 status: 'failed',
                 remark: 'Order canceled!'
             }, {
@@ -481,7 +457,6 @@ exports.cancelOrder = async (req, res, next) => {
                 },
                 transaction: cancelTransaction
             });
-            return cancelOrderResult;
         });
         return res.redirect('/cart/checkout/'+orderId+'/status');
     } catch (e) {
