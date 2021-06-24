ee2c3572320d3bf81c40ce17f6d0d2db6fabea62
Siddhesh Panchal <59734506+srap9499@users.noreply.github.com>
2021-06-24 20:31:52 +0530

    Fixes in model file

--- models/role.model.js
+++ models/role.model.js
@@ -2,8 +2,6 @@
 
 const Sequelize = require("sequelize");
 const { sequelize } = require("../config/db-connection.config");
-const { development: { roles } } = require('../config/development.config');
-
 
 
 const UserRole = sequelize.define('userrole', {
